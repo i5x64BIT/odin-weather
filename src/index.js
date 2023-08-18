64607646a6e568896e31165d897df26f3ac0017e
i5x64BIT <vaintomer@gmail.com>
2023-08-18 16:57:27 +0300

    Add date formatting

--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
 import './style.css';
 import { fetchData } from './util/weather_api';
+import { renderData } from './util/Render';
 
-let data = await fetchData('Tel Aviv');
-const renderData = () => {
-    const locationElem = document.querySelector('.location');
-    locationElem.innerText = `${data.location.name}, ${data.location.country}`;
-
-    const dateElem = document.querySelector('.date');
-    dateElem.innerText = `${data.location.localtime}`;
-
-    const tempElem = document.querySelector('.temperature');
-    tempElem.innerText =`${data.current.temp_c}°C`;
-
-    const weather_iconElem = document.querySelector('.weather-icon');
-    weather_iconElem.src = data.current.condition.icon
-
-    const feels_likeElem = document.querySelector('.temperature +p');
-    feels_likeElem.innerText = `Feels like ${data.current.feelslike_c}°C`;
-
-    const forecastElements = document.querySelectorAll('.forecast tr')
-    for(let i = 0; i < forecastElements.length; i++){
-        forecastElements[i].children[0].innerText = `${data.forecast.forecastday[i+1].date}`;
-        forecastElements[i].children[1].innerText = `${data.forecast.forecastday[i+1].day.maxtemp_c} / ${data.forecast.forecastday[i].day.mintemp_c}`;
-    }
-}
 renderData();
 
 const searchElem = document.querySelector('[type="search"]');
 const handleListener = async function(){
     data = await fetchData(this.value);
-    console.log(data);
     renderData();
 }
 searchElem.addEventListener('submit', await handleListener);
---
+++ src/util/Render.js
@@ -0,0 +1,29 @@
+import { fetchData } from "./weather_api";
+import dateFormat, { masks } from "dateformat";
+
+let data = await fetchData('Tel Aviv');
+const renderData = () => {
+    const locationElem = document.querySelector('.location');
+    locationElem.innerText = `${data.location.name}, ${data.location.country}`;
+
+    const dateElem = document.querySelector('.date');
+    dateElem.innerText = dateFormat(data.location.localtime, "dddd, mmmm dS yyyy");
+
+    const tempElem = document.querySelector('.temperature');
+    tempElem.innerText =`${data.current.temp_c}°C`;
+
+    const weather_iconElem = document.querySelector('.weather-icon');
+    weather_iconElem.src = data.current.condition.icon
+
+    const feels_likeElem = document.querySelector('.temperature +p');
+    feels_likeElem.innerText = `Feels like ${data.current.feelslike_c}°C`;
+
+    const forecastElements = document.querySelectorAll('.forecast tr')
+    for(let i = 0; i < forecastElements.length; i++){
+        forecastElements[i].children[0].innerText = `${dateFormat(data.forecast.forecastday[i+1].date, 'dddd')}`;
+        forecastElements[i].children[1].innerText = `${data.forecast.forecastday[i+1].day.maxtemp_c}° / ${data.forecast.forecastday[i].day.mintemp_c}°`;
+    }
+}
+
+
+export { renderData };
