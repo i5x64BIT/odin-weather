e9f59b3e48b001eda45abd25a9c061ff45ffc27d
i5x64BIT <vaintomer@gmail.com>
2023-08-18 13:49:08 +0300

    Fix request blocking
    Change the  protocol from http to https

--- src/util/weather_api.js
+++ src/util/weather_api.js
@@ -4,7 +4,7 @@ async function fetchData(location) {
     try{
         location = location.toString();
         if(!dataCache || location !== currentLocation){
-            const res = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=4f4fc43807904d4f8e3140150231708&q="${ location }"&days=8&aqi=no&alerts=no`);
+            const res = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4f4fc43807904d4f8e3140150231708&q="${ location }"&days=8&aqi=no&alerts=no`);
             dataCache = await res.json();
             currentLocation = location;
         }
